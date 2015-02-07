a7799c4e703ecdb64832c3dcb1344168479e868d
jcavendish14 <jmc242@case.edu>
2015-02-07 02:31:10 -0500

    Location services

--- app/src/main/java/com/example/jmc242/tigerwolf/MainActivity.java
+++ app/src/main/java/com/example/jmc242/tigerwolf/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.jmc242.tigerwolf;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,8 +17,41 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LocationManager locMan = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+
+        LocationListener locList = new MyLocationListener();
+        locMan.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locList);
     }
 
+    public class MyLocationListener implements LocationListener {
+        @Override
+        public void onLocationChanged(Location loc) {
+            loc.getLatitude();
+            loc.getLongitude();
+
+            String Text = "My current location is: " +
+            "Latidude = " + loc.getLatitude() +
+            "Longitude = " + loc.getLongitude();
+
+            Toast.makeText(getApplicationContext(), Text, Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            Toast.makeText(getApplicationContext(), "GPS Disabled", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            Toast.makeText(getApplicationContext(), "GPS Enabled", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
