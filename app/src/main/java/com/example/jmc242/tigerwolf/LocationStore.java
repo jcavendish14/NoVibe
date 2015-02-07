88bfdae2a5b9212729fed4bbdc001af5cca39fb9
jonathanmonreal <jem177@case.edu>
2015-02-07 05:32:53 -0500

    Made data structure iterable; added remove method.

--- app/src/main/java/com/example/jmc242/tigerwolf/LocationStore.java
+++ app/src/main/java/com/example/jmc242/tigerwolf/LocationStore.java
@@ -1,15 +1,62 @@
 package com.example.jmc242.tigerwolf;
 
 import android.location.Location;
+import java.lang.Iterable;
+import java.util.Iterator;
 
 /**
  * Created by jonmonreal on 2/7/15.
  */
-public class LocationStore {
+public class LocationStore implements Iterable<Location> {
+
     private Node list;
 
     public void addLocation(Location loc) {
         list = new Node(loc, list);
+    }
+
+    @Override
+    public Iterator<Location> iterator() {
+        return new LocationIterator();
+    }
+
+    public void remove(Location loc) {
+        if (list.getLoc().getLongitude() == loc.getLongitude() && list.getLoc().getLatitude() ==
+                loc.getLatitude()) {
+            list = list.getNext();
+        }
+        Node ptr = list.getNext();
+        Node previous = list;
+        while (ptr != null) {
+            if (ptr.getLoc().getLongitude() == loc.getLongitude() && ptr.getLoc().getLatitude() ==
+                    loc.getLatitude()) {
+                previous.setNext(ptr.getNext());
+                return;
+            }
+            previous = ptr;
+            ptr = ptr.getNext();
+        }
+    }
+
+    private class LocationIterator implements Iterator<Location> {
+        private Node current = list;
+
+        @Override
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        @Override
+        public Location next() {
+            Location next = current.getLoc();
+            current = current.getNext();
+            return next;
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
     }
 
     private class Node {
@@ -33,5 +80,12 @@ public class LocationStore {
         private Node getNext() {
             return next;
         }
+
+        private void setNext(Node next) {
+            this.next = next;
+        }
+    }
+
+    public static void main(String[] args) {
     }
 }
