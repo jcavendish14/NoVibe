43595a8013912bc5153c64b7c90725bbb1b5de2b
jcavendish14 <jmc242@case.edu>
2015-02-07 08:14:03 -0500

    Background Location Services

--- app/src/main/java/com/example/jmc242/tigerwolf/MainActivity.java
+++ app/src/main/java/com/example/jmc242/tigerwolf/MainActivity.java
@@ -1,9 +1,7 @@
 package com.example.jmc242.tigerwolf;
 
 import android.location.Location;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.app.Activity;
 import android.widget.Toast;
@@ -12,7 +10,7 @@ import android.content.Context;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
-import com.google.android.gms.drive.Drive;
+import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
@@ -22,22 +20,32 @@ import java.text.DateFormat;
 import java.util.Date;
 
 
-public class MainActivity extends Activity implements ConnectionCallbacks, OnConnectionFailedListener, com.google.android.gms.location.LocationListener {
+public class MainActivity extends Activity implements ConnectionCallbacks, OnConnectionFailedListener, LocationListener {
 
     private boolean reqLocUpd = true;
-    private LocationRequest locReq;
+    protected LocationRequest locReq = LocationRequest.create();
     private GoogleApiClient mGoogleApiClient;
     private Location curLoc;
     private String lastUpdTime;
+    LocationRequest mLocationRequest=new LocationRequest();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        buildGoogleApiClient();
-        createLocationRequest();
-        startLocationUpdates();
-        onLocationChanged(curLoc);
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
     }
 
     protected synchronized void buildGoogleApiClient() {
@@ -73,9 +81,9 @@ public class MainActivity extends Activity implements ConnectionCallbacks, OnCon
         toast.show();
     }
 
-    protected void stopLocationUpdates() {
+    /*protected void stopLocationUpdates() {
         LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
-    }
+    }*/
 
     @Override
     public void onConnected(Bundle connectionHint) {
