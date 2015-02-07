71c1b6144563c78fe2b8c896dbea42f24a70beeb
jcavendish14 <jmc242@case.edu>
2015-02-07 01:05:49 -0500

    Initial commit

---
+++ app/src/androidTest/java/com/example/jmc242/tigerwolf/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.jmc242.tigerwolf;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
