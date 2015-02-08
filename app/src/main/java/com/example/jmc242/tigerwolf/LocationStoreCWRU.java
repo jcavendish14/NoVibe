504e768db4f6ce5842f806020367e001d41effe5
jonathanmonreal <jem177@case.edu>
2015-02-08 12:15:50 -0500

    Added release apk.

--- app/src/main/java/com/example/jmc242/tigerwolf/LocationStoreCWRU.java
+++ app/src/main/java/com/example/jmc242/tigerwolf/LocationStoreCWRU.java
@@ -8,8 +8,12 @@ import android.location.Location;
 public class LocationStoreCWRU extends LocationStore {
     public LocationStoreCWRU() {
         Location t1 = new Location("Test");
+        Location t2 = new Location("Test");
         t1.setLongitude(-81.607898);
         t1.setLatitude(41.502657);
+        t2.setLongitude(-81.607481);
+        t2.setLatitude(41.504162);
         this.addLocation(t1);
+        this.addLocation(t2);
     }
 }
